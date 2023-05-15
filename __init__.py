b7ea45852448c4dccd1012a75a8867ff0c85a8b1
Olubode Olufisayo <61015847+olufisayopython@users.noreply.github.com>
2023-05-15 19:37:04 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'd3c24d998b63bd44f8461431'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
